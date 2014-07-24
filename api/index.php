9bd4b415b32bdeb235fc0e35304188b19ee43816
Charu Hans <charuhans85@gmail.com>
2014-07-23 23:55:18 -0500

    Update index.php
    Jordan's code

--- api/index.php
+++ api/index.php
@@ -22,8 +22,16 @@ $app->get(
 */
 $app->post('/AddScanResults', 'addScanResults');
 
+$app->get('/Vertices', 'getVertices');
+
+$app->get('/Test/:userId', 'getTest');
 
 $app->run();
+
+
+function getTest($userId){
+	echo $userId;
+}
 
 
 /**
@@ -34,27 +42,56 @@ function addScanResults(){
 
     $results = json_decode(Slim::getInstance()->request()->post('scanResults'), true);
     try {
-
-        $insertResult = "INSERT INTO tests(coordinates, direction, reading) VALUE(PointFromText(:coordinates), :direction, :reading)";
-        $db = getConnection();
-        $coordinate = $results['Coordinates'];
-
-        $finalCoordinate = 'POINT(' . $coordinate[0] . ' ' . $coordinate[1] . ')';
-        $direction = $results['Direction'];
-
-        echo $direction;
-        echo $finalCoordinate;
+	    $db = getConnection();
+		$select = "SELECT idVertex FROM Vertices WHERE X=:coordinateX and Y=:coordinateY";
+		$coordinate = $results['Coordinates'];
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("coordinateX", $coordinate[0]);
+		$stmt->bindParam("coordinateY", $coordinate[1]);
+		$stmt->execute();
+		$vertexId = $stmt->fetchAll(PDO::FETCH_OBJ);
+		if(empty($vertexId)){
+			$insertVertex = "INSERT INTO vertices(X, Y) VALUE(:coordinateX, :coordinateY)";
+			$stmt = $db->prepare($insertVertex);
+			$stmt->bindParam("coordinateX", $coordinate[0]);
+			$stmt->bindParam("coordinateY", $coordinate[1]);
+			$stmt->execute();
+			$vertexId = $db->lastInsertId();
+		}
+		else{
+			$vertexId = $vertexId[0]->idVertex;
+		}
+		$insertDirection = "INSERT INTO directiontime(direction) VALUE(:direction)";
+		$stmt = $db->prepare($insertDirection);
+		$stmt->bindParam("direction", $results['Direction']);
+		$stmt->execute();
+		$directionId = $db->lastInsertId();
         foreach ($results['Tests'] as $test) {
-            $stmt = $db->prepare($insertResult);
-
-            $stringTest = json_encode($test);
-
-            $stmt->bindParam("coordinates", $finalCoordinate);
-            $stmt->bindParam("direction", $direction);
-            $stmt->bindParam("reading", $stringTest);
-
-            echo $stringTest;
-            $stmt->execute();
+            foreach($test as $accesspoint){
+				$selectMac = "SELECT idMac FROM mac WHERE macAddress=:address";
+				$stmt = $db->prepare($selectMac);
+				$stmt->bindParam("address", $accesspoint['Accesspoint']);
+				$stmt->execute();
+				$macId = $stmt->fetchAll(PDO::FETCH_OBJ);
+				if(empty($macId)){
+					$insertMac = "INSERT INTO mac(macAddress) VALUE(:address)";
+					$stmt = $db->prepare($insertMac);
+					$stmt->bindParam("address", $accesspoint['Accesspoint']);
+					$stmt->execute();
+					$macId = $db->lastInsertId();
+				}
+				else{
+					$macId = $macId[0]->idMac;
+				}
+				$finalInsert = "INSERT INTO data(idVertex, idMac, idDirectionTime, strength) VALUE(:vertex, :mac, :direction, :strength)";
+				$stmt = $db->prepare($finalInsert);
+				$stmt->bindParam("vertex", $vertexId);
+				$stmt->bindParam("mac", $macId);
+				$stmt->bindParam("direction", $directionId);
+				$stmt->bindParam("strength", $accesspoint['SignalStrength']);
+				$stmt->execute();
+			
+			}
         }
 
         $db = null;
@@ -63,6 +100,15 @@ function addScanResults(){
     }   
 }
 
+function getVertices(){
+	$db = getConnection();
+	$select = "SELECT X, Y FROM Vertices";
+	$stmt = $db->prepare($select);
+	$stmt->execute();
+	$vertices = $stmt->fetchAll(PDO::FETCH_OBJ);
+	echo '{"Vertices": ' . json_encode($vertices) . '}';
+	$db = null;
+}
 
 /**
 * A function that sets up the connection to the database
@@ -70,7 +116,7 @@ function addScanResults(){
 function getConnection() {
     $dbhost="localhost";
     $dbuser="root";
-    $dbpass="";
+    $dbpass="0000";
     $dbname="accesspoint";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
