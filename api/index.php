28da8b9e93385ab97339d4c00a23d29d8c54be07
JKayse <jkayse@smu.edu>
2014-07-26 20:45:32 -0700

    Added functions to get average access point strength with and without direction

--- api/index.php
+++ api/index.php
@@ -28,6 +28,12 @@ $app->get('/Test/:userId', 'getTest');
 
 $app->get('/Data', 'getAllData');
 
+$app->get('/DataAverage', 'getDataAverage');
+
+$app->get('/DataNoDirection', 'getDataNoDirection');
+
+
+
 $app->run();
 
 
@@ -260,6 +266,169 @@ function getAllData(){
 			echo']}';
 		}
 
+		echo ']}';
+	}
+	echo ']}';
+	$db = null;
+}
+
+
+function getDataAverage(){
+	$db = getConnection();
+	$select = "SELECT idVertex, X, Y FROM Vertices";
+	$stmt = $db->prepare($select);
+	$stmt->execute();
+	$vertices = $stmt->fetchAll(PDO::FETCH_OBJ);
+	echo '{"Vertices": [';
+	$i = 0;
+	foreach ($vertices as $vertex) {
+		if($i != 0) {
+			echo ',';
+		} else {
+			$i++;
+		}
+		echo '{"X": ' . $vertex->X . ', "Y": ' . $vertex->Y . ', "Directions": {';
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='N'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$north = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '"North": [';
+		$i = 0;
+		foreach ($north as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "AverageStrength": ';
+			$select = "SELECT AVG(strength) as strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='N' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			echo $strengths[0]->strength. '}';
+		}
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='E'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$east = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '], "East": [';
+		$i = 0;
+		foreach ($east as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "AverageStrength": ';
+			$select = "SELECT AVG(strength) as strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='E' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			echo $strengths[0]->strength. '}';
+		}
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='S'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$south = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '], "South": [';
+		$i = 0;
+		foreach ($south as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "AverageStrength": ';
+			$select = "SELECT AVG(strength) as strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='S' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			echo $strengths[0]->strength. '}';
+		}
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='W'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$west = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '], "West": [';
+		$i = 0;
+		foreach ($west as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "AverageStrength": ';
+			$select = "SELECT AVG(strength) as strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='W' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			echo $strengths[0]->strength. '}';
+		}
+
+		echo ']}';
+	}
+	echo ']}';
+	$db = null;
+}
+
+function getDataNoDirection(){
+	$db = getConnection();
+	$select = "SELECT idVertex, X, Y FROM Vertices";
+	$stmt = $db->prepare($select);
+	$stmt->execute();
+	$vertices = $stmt->fetchAll(PDO::FETCH_OBJ);
+	echo '{"Vertices": [';
+	$i = 0;
+	foreach ($vertices as $vertex) {
+		if($i != 0) {
+			echo ',';
+		} else {
+			$i++;
+		}
+		echo '{"X": ' . $vertex->X . ', "Y": ' . $vertex->Y . ', "AccessPoints": [';
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$accesspoints = $stmt->fetchAll(PDO::FETCH_OBJ);
+		$i = 0;
+		foreach ($accesspoints as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "AverageStrength": ';
+			$select = "SELECT AVG(strength) as strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex  and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId  and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			echo $strengths[0]->strength. '}';
+		}
 		echo ']}';
 	}
 	echo ']}';
