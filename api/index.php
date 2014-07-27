1a03754c5e5b1292a49e9bf8df34e385a91d7eef
JKayse <jkayse@smu.edu>
2014-07-26 20:13:17 -0700

    Added function to get all data

--- api/index.php
+++ api/index.php
@@ -26,6 +26,8 @@ $app->get('/Vertices', 'getVertices');
 
 $app->get('/Test/:userId', 'getTest');
 
+$app->get('/Data', 'getAllData');
+
 $app->run();
 
 
@@ -51,7 +53,7 @@ function addScanResults(){
 		$stmt->execute();
 		$vertexId = $stmt->fetchAll(PDO::FETCH_OBJ);
 		if(empty($vertexId)){
-			$insertVertex = "INSERT INTO vertices(X, Y) VALUE(:coordinateX, :coordinateY)";
+			$insertVertex = "INSERT INTO Vertices(X, Y) VALUE(:coordinateX, :coordinateY)";
 			$stmt = $db->prepare($insertVertex);
 			$stmt->bindParam("coordinateX", $coordinate[0]);
 			$stmt->bindParam("coordinateY", $coordinate[1]);
@@ -61,29 +63,29 @@ function addScanResults(){
 		else{
 			$vertexId = $vertexId[0]->idVertex;
 		}
-		$insertDirection = "INSERT INTO directiontime(direction) VALUE(:direction)";
+		$insertDirection = "INSERT INTO DirectionTime(direction) VALUE(:direction)";
 		$stmt = $db->prepare($insertDirection);
 		$stmt->bindParam("direction", $results['Direction']);
 		$stmt->execute();
 		$directionId = $db->lastInsertId();
         foreach ($results['Tests'] as $test) {
             foreach($test as $accesspoint){
-				$selectMac = "SELECT idMac FROM mac WHERE macAddress=:address";
+				$selectMac = "SELECT idMac FROM Mac WHERE macAddress=:address";
 				$stmt = $db->prepare($selectMac);
-				$stmt->bindParam("address", $accesspoint['Accesspoint']);
+				$stmt->bindParam("address", $accesspoint['AccessPoint']);
 				$stmt->execute();
 				$macId = $stmt->fetchAll(PDO::FETCH_OBJ);
 				if(empty($macId)){
-					$insertMac = "INSERT INTO mac(macAddress) VALUE(:address)";
+					$insertMac = "INSERT INTO Mac(macAddress) VALUE(:address)";
 					$stmt = $db->prepare($insertMac);
-					$stmt->bindParam("address", $accesspoint['Accesspoint']);
+					$stmt->bindParam("address", $accesspoint['AccessPoint']);
 					$stmt->execute();
 					$macId = $db->lastInsertId();
 				}
 				else{
 					$macId = $macId[0]->idMac;
 				}
-				$finalInsert = "INSERT INTO data(idVertex, idMac, idDirectionTime, strength) VALUE(:vertex, :mac, :direction, :strength)";
+				$finalInsert = "INSERT INTO Data(idVertex, idMac, idDirectionTime, strength) VALUE(:vertex, :mac, :direction, :strength)";
 				$stmt = $db->prepare($finalInsert);
 				$stmt->bindParam("vertex", $vertexId);
 				$stmt->bindParam("mac", $macId);
@@ -110,13 +112,168 @@ function getVertices(){
 	$db = null;
 }
 
+function getAllData(){
+	$db = getConnection();
+	$select = "SELECT idVertex, X, Y FROM Vertices";
+	$stmt = $db->prepare($select);
+	$stmt->execute();
+	$vertices = $stmt->fetchAll(PDO::FETCH_OBJ);
+	echo '{"Vertices": [';
+	$i = 0;
+	foreach ($vertices as $vertex) {
+		if($i != 0) {
+			echo ',';
+		} else {
+			$i++;
+		}
+		echo '{"X": ' . $vertex->X . ', "Y": ' . $vertex->Y . ', "Directions": {';
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='N'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$north = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '"North": [';
+		$i = 0;
+		foreach ($north as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "Strengths": [';
+			$select = "SELECT strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='N' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$i = 0;
+			foreach ($strengths as $strength) {
+				if($i != 0) {
+				echo ',';
+				} else {
+					$i++;
+				}
+				echo $strength->strength;
+			}
+			echo']}';
+		}
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='E'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$east = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '], "East": [';
+		$i = 0;
+		foreach ($east as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "Strengths": [';
+			$select = "SELECT strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='E' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$i = 0;
+			foreach ($strengths as $strength) {
+				if($i != 0) {
+				echo ',';
+				} else {
+					$i++;
+				}
+				echo $strength->strength;
+			}
+			echo']}';
+		}
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='S'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$south = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '], "South": [';
+		$i = 0;
+		foreach ($south as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "Strengths": [';
+			$select = "SELECT strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='S' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$i = 0;
+			foreach ($strengths as $strength) {
+				if($i != 0) {
+				echo ',';
+				} else {
+					$i++;
+				}
+				echo $strength->strength;
+			}
+			echo']}';
+		}
+		$select = "SELECT distinct Data.idMac, macAddress from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='W'";
+		$stmt = $db->prepare($select);
+		$stmt->bindParam("vertexId", $vertex->idVertex);
+		$stmt->execute();
+		$west = $stmt->fetchAll(PDO::FETCH_OBJ);
+		echo '], "West": [';
+		$i = 0;
+		foreach ($west as $mac) {
+			if($i != 0) {
+			echo ',';
+			} else {
+				$i++;
+			}
+			echo '{"AccessPoint": "';
+			echo $mac->macAddress;
+			echo '", "Strengths": [';
+			$select = "SELECT strength from Data join DirectionTime join Mac join Vertices where Data.idDIrectionTime=DirectionTime.idDirectionTime and Data.idVertex=Vertices.idVertex and Data.idMac=Mac.idMac and Vertices.idVertex=:vertexId and DirectionTime.direction='W' and Mac.idMac=:macId";
+			$stmt = $db->prepare($select);
+			$stmt->bindParam("vertexId", $vertex->idVertex);
+			$stmt->bindParam("macId", $mac->idMac);
+			$stmt->execute();
+			$strengths = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$i = 0;
+			foreach ($strengths as $strength) {
+				if($i != 0) {
+				echo ',';
+				} else {
+					$i++;
+				}
+				echo $strength->strength;
+			}
+			echo']}';
+		}
+
+		echo ']}';
+	}
+	echo ']}';
+	$db = null;
+}
+
+
 /**
 * A function that sets up the connection to the database
 */
 function getConnection() {
     $dbhost="localhost";
     $dbuser="root";
-    $dbpass="0000";
+    $dbpass="";
     $dbname="accesspoint";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);  
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
